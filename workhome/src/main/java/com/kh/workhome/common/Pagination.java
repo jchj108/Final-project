96cfc669c8f3ec23bc11c175801e49f02ddec09a
jchj108 <jchj108@naver.com>
2021-08-16 15:55:45 +0900

    임시보관함 보기

--- workhome/src/main/java/com/kh/workhome/common/Pagination.java
+++ workhome/src/main/java/com/kh/workhome/common/Pagination.java
@@ -16,4 +16,18 @@ public class Pagination {
 		
 		return pi;
 	}
+	
+	public static PageInfo getPageInfo(int currentPage, int listCount, int boardLimit) { // 메일용 pagination
+		int pageLimit=10;
+		int maxPage= (int)Math.ceil((double)listCount / boardLimit);
+		int startPage=  ((currentPage-1)/pageLimit) * pageLimit + 1;
+		int endPage = startPage + pageLimit -1;
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		PageInfo pi = new PageInfo(currentPage, listCount, pageLimit, boardLimit, maxPage, startPage, endPage);
+		
+		return pi;
+	}
 }
--- workhome/src/main/java/com/kh/workhome/mail/model/vo/Mail.java
+++ workhome/src/main/java/com/kh/workhome/mail/model/vo/Mail.java
@@ -1,12 +1,11 @@
 package com.kh.workhome.mail.model.vo;
 
 import java.sql.Date;
-
-import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
 
 public class Mail {
 
-	private int number;
+	private int mailNo;
 	private String receiveEmp; // 받는 사람
 	private String etype; // 외부, 내부
 	private String etitle;
@@ -14,90 +13,17 @@ public class Mail {
 	private String estatus;
 	private Date eRTime; // 읽은 시간
 	private Date sDate; // 보낸 시간
+	private String empNo;
 	private String senderMailId; // 보낸 사람 메일 주소
-	private String empNo; // FK
+	private String senderName;
+	private ArrayList<MailFile> mailFileList;
 	
-	public Mail() {
-	}
-
-	public int getNumber() {
-		return number;
-	}
-
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
-	public String getReceiveEmp() {
-		return receiveEmp;
-	}
-
-	public void setReceiveEmp(String receiveEmp) {
-		this.receiveEmp = receiveEmp;
-	}
-
-	public String getEtype() {
-		return etype;
-	}
-
-	public void setEtype(String etype) {
-		this.etype = etype;
-	}
-
-	public String getEtitle() {
-		return etitle;
-	}
-
-	public void setEtitle(String etitle) {
-		this.etitle = etitle;
-	}
-
-	public String getEcontent() {
-		return econtent;
-	}
-
-	public void setEcontent(String econtent) {
-		this.econtent = econtent;
-	}
-
-	public String getEstatus() {
-		return estatus;
-	}
-
-	public void setEstatus(String estatus) {
-		this.estatus = estatus;
-	}
-
-	public Date geteRTime() {
-		return eRTime;
-	}
-
-	public void seteRTime(Date eRTime) {
-		this.eRTime = eRTime;
-	}
-
-	public Date getsDate() {
-		return sDate;
-	}
-
-	public void setsDate(Date sDate) {
-		this.sDate = sDate;
-	}
-
-	public String getSenderMailId() {
-		return senderMailId;
-	}
-
-	public void setSenderMailId(String senderMailId) {
-		this.senderMailId = senderMailId;
-	}
-
-
-
-	public Mail(int number, String receiveEmp, String etype, String etitle, String econtent, String estatus,
-			Date eRTime, Date sDate, String senderMailId, String empNo) {
+	public Mail() {}
+	
+	public Mail(int mailNo, String receiveEmp, String etype, String etitle, String econtent, String estatus,
+			Date eRTime, Date sDate, String senderMailId, String senderName, ArrayList<MailFile> mailFileList) {
 		super();
-		this.number = number;
+		this.mailNo = mailNo;
 		this.receiveEmp = receiveEmp;
 		this.etype = etype;
 		this.etitle = etitle;
@@ -106,7 +32,8 @@ public class Mail {
 		this.eRTime = eRTime;
 		this.sDate = sDate;
 		this.senderMailId = senderMailId;
-		this.empNo = empNo;
+		this.senderName = senderName;
+		this.mailFileList = mailFileList;
 	}
 
 	public String getEmpNo() {
@@ -116,13 +43,92 @@ public class Mail {
 	public void setEmpNo(String empNo) {
 		this.empNo = empNo;
 	}
+	
+	
 
-	@Override
-	public String toString() {
-		return "Mail [number=" + number + ", receiveEmp=" + receiveEmp + ", etype=" + etype + ", etitle=" + etitle
-				+ ", econtent=" + econtent + ", estatus=" + estatus + ", eRTime=" + eRTime + ", sDate=" + sDate
-				+ ", senderMailId=" + senderMailId + ", empNo=" + empNo + "]";
+	public Mail(int mailNo, String receiveEmp, String etype, String etitle, String econtent, String estatus,
+			Date eRTime, Date sDate, String empNo, String senderMailId, String senderName,
+			ArrayList<MailFile> mailFileList) {
+		super();
+		this.mailNo = mailNo;
+		this.receiveEmp = receiveEmp;
+		this.etype = etype;
+		this.etitle = etitle;
+		this.econtent = econtent;
+		this.estatus = estatus;
+		this.eRTime = eRTime;
+		this.sDate = sDate;
+		this.empNo = empNo;
+		this.senderMailId = senderMailId;
+		this.senderName = senderName;
+		this.mailFileList = mailFileList;
 	}
 
+	public int getMailNo() {
+		return mailNo;
+	}
+	public void setMailNo(int mailNo) {
+		this.mailNo = mailNo;
+	}
+	public String getReceiveEmp() {
+		return receiveEmp;
+	}
+	public void setReceiveEmp(String receiveEmp) {
+		this.receiveEmp = receiveEmp;
+	}
+	public String getEtype() {
+		return etype;
+	}
+	public void setEtype(String etype) {
+		this.etype = etype;
+	}
+	public String getEtitle() {
+		return etitle;
+	}
+	public void setEtitle(String etitle) {
+		this.etitle = etitle;
+	}
+	public String getEcontent() {
+		return econtent;
+	}
+	public void setEcontent(String econtent) {
+		this.econtent = econtent;
+	}
+	public String getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	public Date geteRTime() {
+		return eRTime;
+	}
+	public void seteRTime(Date eRTime) {
+		this.eRTime = eRTime;
+	}
+	public Date getsDate() {
+		return sDate;
+	}
+	public void setsDate(Date sDate) {
+		this.sDate = sDate;
+	}
+	public String getSenderMailId() {
+		return senderMailId;
+	}
+	public void setSenderMailId(String senderMailId) {
+		this.senderMailId = senderMailId;
+	}
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public ArrayList<MailFile> getMailFileList() {
+		return mailFileList;
+	}
+	public void setMailFileList(ArrayList<MailFile> mailFileList) {
+		this.mailFileList = mailFileList;
+	}
 
 }
